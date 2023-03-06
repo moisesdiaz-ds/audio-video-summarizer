588c33dc8634d96b7c591b927def3dd53183d746
moises_df <moisesintec@gmail.com>
2023-03-05 20:04:16 -0400

    Adaptado para internet 3

--- app.py
+++ app.py
@@ -17,6 +17,42 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
 
+
+    def divide_by_chunks(file_path):
+        # set the path to the mp3 file
+        filename = file_path.split('\\')[-1].split('/')[-1]
+        filename = "".join(filename.split('.')[:-1])
+        audio_path = file_path
+
+        # set the length of each chunk in milliseconds (10 minutes = 600000 milliseconds)
+        chunk_length_ms = 1800000
+
+        # create an AudioSegment object from the mp3 file
+        audio = AudioSegment.from_file(audio_path)
+
+        # get the length of the audio in milliseconds
+        audio_length_ms = len(audio)
+
+        # determine the number of chunks needed
+        num_chunks = int(audio_length_ms / chunk_length_ms) + 1
+
+        # create a directory to store the chunks
+        output_dir = 'chunks'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        file_chunks_list = []
+        # split the audio into chunks and save each chunk as a separate mp3 file
+        for i in range(num_chunks):
+            start = i * chunk_length_ms
+            end = min((i + 1) * chunk_length_ms, audio_length_ms)
+            chunk = audio[start:end]
+            chunk_file = f"{output_dir}/{filename}_chunk_{i+1}.mp3"
+            chunk.export(chunk_file, format="mp3")
+            file_chunks_list.append(chunk_file)
+
+        return file_chunks_list
+
     def is_video_file(file_path):
         try:
             video = VideoFileClip(file_path)
@@ -130,10 +166,18 @@ def upload():
     #command = f"whisper {file_path} --task transcribe --model medium --verbose False --device cuda --output_dir audio_transcription"
     #subprocess.run(command, shell=True)
 
+
     # VERSION WHISPER API
-    audio_file= open(file_path, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    transcript = transcript['text']
+    file_chunks_list = divide_by_chunks(file_path) # Esto es necesario porque la version de api tiene un limite
+    transcript_all = []
+    for f in file_chunks_list:
+        
+        audio_file= open(file_path, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        transcript = transcript['text']
+        transcript_all.append(transcript)
+
+    transcript = "".join(transcript_all)
 
     with open("audio_transcription/"+filename+".txt", 'w',encoding="utf-8") as f:
         f.write(transcript)
