793e18112f54037909e294cd1d5c0aac5570bf94
moise <moise@MOISES>
2023-03-06 22:46:05 -0400

    new version

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from pydub import AudioSegment
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,7 +27,7 @@ def upload():
         audio_path = file_path
 
         # set the length of each chunk in milliseconds (10 minutes = 600000 milliseconds)
-        chunk_length_ms = 1800000
+        chunk_length_ms = 1400000
 
         # create an AudioSegment object from the mp3 file
         audio = AudioSegment.from_file(audio_path)
@@ -76,7 +78,7 @@ def upload():
         # Define the text to be summarized
         text = open(f"{file_path}", "r",encoding="utf-8").read()
 
-        limit = 50000
+        limit = 12000
         loops = (len(text)//limit)+1
         message_list = []
         for n in range(loops):
@@ -89,8 +91,9 @@ def upload():
             # Define the prompt for summarization
             #prompt = f"El siguiente texto es una conversacion, porfavor resumelo y enumera los puntos mas importantes, es importante que no falte ningun tema mencionado:\n\n{t}"
             prompt = f"""
-            Haz todo lo posible por desarrollar el siguiente texto como si fuera una minuta. Es importante ENUMERAR y DESARROLLAR a plenitud cada uno de los temas sin que falte ningun punto importante de lo tratado. Repito es IMPORTANTE que no falte ninguno de los temas tratados
-
+            Haz todo lo posible por desarrollar el siguiente texto. Es importante ENUMERAR y DESARROLLAR 
+            a plenitud cada uno de los temas sin que falte ningun punto importante de lo tratado. Repito es IMPORTANTE 
+            que no falte ninguno de los temas tratados
             Texto:
             {t}
             """
@@ -109,14 +112,68 @@ def upload():
             message_final = "".join(message_list)
 
         else:
-            t2 = "\n\n".join(message_list)
+            t2 = "\n\n=== Minuta:\n".join(message_list)
             # Define the prompt for summarization
             #prompt = f"Porfavor combina estos {len(message_list)} resumenes en uno solo y los puntos enumerados en una sola lista, es importante que no falte ningun tema mencionado:\n\n{t2}"
 
             prompt = f"""
-            Haz todo lo posible por desarrollar el siguiente texto como si fuera una minuta. Es importante ENUMERAR y DESARROLLAR a plenitud cada uno de los temas sin que falte ningun punto importante de lo tratado. Repito es IMPORTANTE que no falte ninguno de los temas tratados
+            Haz todo lo posible por combinar estos textos y convertirlos en una minuta. 
+            Es IMPORTANTE que no falte ninguno de los temas tratados.
 
-            Texto:
+            Aqui te dejo un ejemplo del que te puedes guiar
+
+            ========= Ejemplo:
+            Minuta de Asamblea [a completar]
+
+            Fecha: [a completar]
+
+            Hora de inicio: [a completar]
+
+            Lugar: Virtual a través de Zoom y YouTube
+
+            Asistentes: Todos los miembros del consejo, propietarios de condominios
+
+            Punto 1. Bienvenida y agradecimiento
+
+            Se da la bienvenida a todos los propietarios de condominios y se agradece por asistir a la asamblea vía Zoom debido a la pandemia de COVID-19. Los miembros del consejo presentes son el tesorero, el secretario Armando de León, Christopher, George y la presidenta Norma Miranda. También se agradece al señor Jeff por su ayuda en la traducción al inglés.
+
+            Punto 2. Logros del año pasado (2021)
+
+            - Remodelación de pasillos en piedra alha roja detrás de la oficina de la Torre D y en la salida de la puerta del cuarto de máquinas.
+            - Remodelación de la escalera de la entrada de la Torre B.
+            - Sellado de la piedra roja en todos los pasillos de aires comunes.
+            - Instalación del techo de la entrada del condominio para proteger el estacionamiento.
+            - Construcción de un nuevo baño para las empleadas.
+            - Reparación del tejado y cambio de algunas vigas en la oficina de la administración.
+            - Reparación de varias fugas de agua y gas en diferentes áreas del condominio.
+
+            Punto 3. Programa de impermeabilización de techos y pintura
+
+            - Instalación del manto prefabricado para la impermeabilización en 6 departamentos.
+            - Pintura de todo el edificio B y áreas comunes del condominio.
+            - Finalización del programa de impermeabilización de techos este año (2022).
+
+
+            Punto 5. Agradecimiento
+
+            Se agradece a todos los propietarios por su compromiso en mantener sus mantenimientos al día.
+
+            Punto 6. Verificación del quórum legal para la asamblea
+
+            Se verificó el quórum legal para la asamblea y se determinó que el 53% de los propietarios de condominios votaron.
+
+            Punto 7. Discusión y aprobación del presupuesto de ingresos y egresos para este año
+
+            El 53% de los propietarios de condominios aprobaron la cuota de mantenimiento de este año.
+
+            Punto 8. Discusión y aprobación de continuar el mismo consejo de administración
+
+            La gráfica mostró que la mayoría de los propietarios votaron para continuar con el mismo consejo de administración para el próximo año.
+
+            Hora de finalización: [a completar]
+
+
+            ========= Textos:
             {t2}
             """
 
@@ -197,5 +254,7 @@ def upload():
     return render_template('received.html', message_final=message_final)
 
 if __name__ == '__main__':
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = datetime.timedelta(minutes=30)
+
     app.run(host='0.0.0.0')
     #app.run()
