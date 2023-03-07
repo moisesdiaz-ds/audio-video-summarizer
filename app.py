e406643f1025ad499bfa08acba066e2dfeb47a3f
moise <moise@MOISES>
2023-03-06 23:02:25 -0400

    new version

--- app.py
+++ app.py
@@ -184,7 +184,8 @@ def upload():
                     {"role": "user", "content":prompt}])
             message_final =completions.choices[0].message.content
 
-        with open(f"results/{file_path.split('.')[0]}.txt".split('\\')[-1].split('/')[-1], 'w') as f:
+        filename_res = file_path.split('.')[0].split('\\')[-1].split('/')[-1]
+        with open(f"results/{filename_res}.txt", 'w') as f:
             f.write(message_final)
 
         print(message_final)
